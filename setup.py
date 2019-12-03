b5c274442f5a2fdd5872de79c0ad70f758789fe1
Christine Hsu <whayin.hsu@gmail.com>
2019-12-02 22:23:08 -0800

    Update README.md and setup.py with minor changes

--- setup.py
+++ setup.py
@@ -5,3 +5,19 @@ with open('README.md') as f:
 
 with open('LICENSE') as f:
 	license = f.read()
+
+with open('requirements.txt') as f:
+	requirements = f.read()
+
+setup(
+	name='dicom_data_preprocess',
+	version='0.0.2',
+	description='The dicom data preprocessing block of the Medical Image Segmentation Analysis Pipeline',
+	longdescription=readme,
+	author='Christine Hsu',
+	author_email='christine.wy.hsu@gmail.com',
+	url='https://github.com/hsuchristine',
+	license=license,
+	packages=find_packages(exclude=('tests')),
+	install_required=requirements
+	)
